35be32d6fb92fc4b24d85239e7765c00f1af2dfa
Eran Gil <eran.gil2@gmail.com>
2021-06-08 23:22:36 +0300

    Initial commit

---
+++ CovidDataLake.DataLake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CovidDataLake.DataLake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
