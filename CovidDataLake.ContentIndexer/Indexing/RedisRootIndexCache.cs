a9e85b93a731d5de69b153c5bba361ef1ebd7029
Eran Gil <eran.gil2@gmail.com>
2021-07-24 10:36:45 +0300

    Better locking mechanism

--- CovidDataLake.ContentIndexer/Indexing/RedisRootIndexCache.cs
+++ CovidDataLake.ContentIndexer/Indexing/RedisRootIndexCache.cs
@@ -10,7 +10,8 @@ namespace CovidDataLake.ContentIndexer.Indexing
     {
         private readonly IConnectionMultiplexer _connection;
         private readonly TimeSpan _lockTimeSpan = TimeSpan.FromSeconds(10);
-        private const string RedisLockKey = ":ROOT_INDEX_LOCK:";
+        private const string RedisKeyPrefix = "ROOT_INDEX::";
+        private const string RedisLockKeyPrefix = "ROOT_INDEX_LOCK::";
         public RedisRootIndexCache(IConnectionMultiplexer connection)
         {
             _connection = connection;
@@ -22,16 +23,12 @@ namespace CovidDataLake.ContentIndexer.Indexing
             foreach (var (key, indexRows) in columnMappings)
             {
                 var redisKey = GetRedisKeyForColumn(key);
+                var redisLockKey = GetRedisLockKeyForColumn(key);
                 var sortedSetEntries = indexRows.Select(CreateSortedSetEntryFromColumnMapping).ToArray();
-                await db.LockTakeAsync(RedisLockKey, 1, _lockTimeSpan);
+                await db.LockTakeAsync(redisLockKey, 1, _lockTimeSpan);
                 await db.SortedSetAddAsync(redisKey, sortedSetEntries);
-                await db.LockReleaseAsync(RedisLockKey, 0);
+                await db.LockReleaseAsync(redisLockKey, 0);
             }
-        }
-
-        private static SortedSetEntry CreateSortedSetEntryFromColumnMapping(RootIndexRow mapping)
-        {
-            return new(mapping.FileName, mapping.Max);
         }
 
         public async Task<string> GetFileNameForColumnAndValue(string column, ulong val)
@@ -39,17 +36,28 @@ namespace CovidDataLake.ContentIndexer.Indexing
             var db = _connection.GetDatabase();
             //todo: need to test what happens when there's no index yet for column or for value...
             var redisKey = GetRedisKeyForColumn(column);
-            await db.LockTakeAsync(RedisLockKey, 1, _lockTimeSpan);
+            var redisLockKey = GetRedisLockKeyForColumn(column);
+            await db.LockTakeAsync(redisLockKey, 1, _lockTimeSpan);
             var indexFileValue = (await db.SortedSetRangeByScoreAsync(redisKey, val, take: 1)).FirstOrDefault();
-            await db.LockReleaseAsync(RedisLockKey, 0);
+            await db.LockReleaseAsync(redisLockKey, 0);
             if (indexFileValue == default) return null;
             var indexFileName = indexFileValue.ToString();
             return indexFileName;
         }
 
+        private static SortedSetEntry CreateSortedSetEntryFromColumnMapping(RootIndexRow mapping)
+        {
+            return new(mapping.FileName, mapping.Max);
+        }
+
         private static string GetRedisKeyForColumn(string column)
         {
-            return $"ROOT_INDEX::{column}";
+            return $"{RedisKeyPrefix}{column}";
+        }
+        
+        private static string GetRedisLockKeyForColumn(string column)
+        {
+            return $"{RedisLockKeyPrefix}{column}";
         }
     }
 }
