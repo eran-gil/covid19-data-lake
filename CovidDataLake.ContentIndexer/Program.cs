79b27a02a4c75d4a1b88988770b804e63b30e3ac
Eran Gil <eran.gil2@gmail.com>
2021-07-31 11:15:01 +0300

    Configured the dependency injection

--- CovidDataLake.ContentIndexer/Program.cs
+++ CovidDataLake.ContentIndexer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using CovidDataLake.Amazon;
 using CovidDataLake.Common;
 using CovidDataLake.Common.Hashing;
+using CovidDataLake.Common.Locking;
 using CovidDataLake.ContentIndexer.Extraction;
 using CovidDataLake.ContentIndexer.Indexing;
 using CovidDataLake.ContentIndexer.Orchestration;
@@ -8,6 +10,7 @@ using CovidDataLake.Kafka.Consumer;
 using CovidDataLake.Kafka.Consumer.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace CovidDataLake.ContentIndexer
 {
@@ -19,11 +22,22 @@ namespace CovidDataLake.ContentIndexer
             IServiceCollection serviceCollection = new ServiceCollection();
             serviceCollection.AddLogging();
             serviceCollection.BindConfigurationToContainer<KafkaConsumerConfiguration>(configuration, "Kafka");
+            var redisConfig = new ConfigurationOptions();
+            var redisConnection = await ConnectionMultiplexer.ConnectAsync(redisConfig);
+            serviceCollection.AddSingleton<IConnectionMultiplexer>(redisConnection);
             serviceCollection.AddSingleton<IConsumerFactory, KafkaConsumerFactory>();
             serviceCollection.AddSingleton<IOrchestrator, KafkaOrchestrator>();
             serviceCollection.AddSingleton<IStringHash, Md5StringHash>();
             serviceCollection.AddSingleton<IIndexFileWriter, AmazonIndexFileWriter>();
             serviceCollection.AddSingleton<IFileTableWrapperFactory, CsvFileTableWrapperFactory>();
+            serviceCollection.AddSingleton<IContentIndexer, NeedleInHaystackContentIndexer>();
+            serviceCollection.AddSingleton<IStringHash, Md5StringHash>();
+            serviceCollection.AddSingleton<IIndexFileWriter, AmazonIndexFileWriter>();
+            serviceCollection.AddSingleton<IRootIndexAccess, AmazonRootIndexFileAccess>();
+            serviceCollection.AddSingleton<IRootIndexCache, RedisRootIndexCache>();
+            serviceCollection.AddSingleton<IAmazonAdapter, AmazonClientAdapter>();
+            serviceCollection.AddSingleton<ILock, RedisLock>();
+            
             var serviceProvider = serviceCollection.BuildServiceProvider();
             var orchestrator = serviceProvider.GetService<IOrchestrator>();
             await orchestrator.StartOrchestration();
