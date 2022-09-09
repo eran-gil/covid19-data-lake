ab42b6b80be4fc7191e0d384fb63c47ff32f550b
Eran Gil <eran.gil2@gmail.com>
2022-09-09 18:31:04 +0300

    chore(content-indexer): removed unused dependencies

--- CovidDataLake.MetadataIndexer/Extraction/Configuration/MetadataExtractorConfiguration.cs
+++ CovidDataLake.MetadataIndexer/Extraction/Configuration/MetadataExtractorConfiguration.cs
@@ -2,6 +2,8 @@
 {
     public class MetadataExtractorConfiguration
     {
+#pragma warning disable CS8618
         public IEnumerable<string> IgnoreMetadataKeys { get; set; }
+#pragma warning restore CS8618
     }
 }
