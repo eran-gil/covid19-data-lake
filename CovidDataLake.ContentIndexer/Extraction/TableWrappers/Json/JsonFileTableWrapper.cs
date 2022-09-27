a0d339ee637e5a8cdfafbeb52913a61453c1c636
Eran Gil <eran.gil2@gmail.com>
2022-09-27 11:40:38 +0300

    fix(content-indexer): allowed more formats of json files

--- CovidDataLake.ContentIndexer/Extraction/TableWrappers/Json/JsonFileTableWrapper.cs
+++ CovidDataLake.ContentIndexer/Extraction/TableWrappers/Json/JsonFileTableWrapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using CovidDataLake.ContentIndexer.Extraction.Models;
@@ -21,19 +22,49 @@ class JsonFileTableWrapper : IFileTableWrapper
     public string Filename { get; set; }
     public IEnumerable<KeyValuePair<string, IEnumerable<RawEntry>>> GetColumns()
     {
-        var columns = new HashSet<string>();
-        using (var fileStream = File.OpenRead(Filename))
-        using (var reader = new StreamReader(fileStream))
+        try
         {
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                var currentItem = JSON.Deserialize<Dictionary<string, object>>(line);
-                columns.UnionWith(currentItem.Keys);
-            }
+            var columns = GetColumnNames();
+            return columns.ToDictionary(col => col, GetColumnValues);
+        }
+        catch (Exception ex)
+        {
+            return Enumerable.Empty<KeyValuePair<string, IEnumerable<RawEntry>>>();
         }
 
-        return columns.ToDictionary(col => col, GetColumnValues);
+    }
+
+    private IEnumerable<string> GetColumnNames()
+    {
+        using var fileStream = File.OpenRead(Filename);
+        using var reader = new StreamReader(fileStream);
+        var columns = new HashSet<string>();
+        var firstChar = (char)reader.Read();
+        reader.DiscardBufferedData();
+        fileStream.Seek(0, SeekOrigin.Begin);
+        var items = firstChar == '[' ? GetItemsByArray(reader) : GetItemsByRow(reader);
+
+        foreach (var item in items)
+        {
+            columns.UnionWith(item.Keys);
+        }
+
+        return columns;
+    }
+
+    private static IEnumerable<Dictionary<string, object>> GetItemsByArray(StreamReader reader)
+    {
+        return JSON.Deserialize<List<Dictionary<string, object>>>(reader.ReadToEnd());
+    }
+
+    private static IEnumerable<Dictionary<string, object>> GetItemsByRow(StreamReader reader)
+    {
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            var currentItem = JSON.Deserialize<Dictionary<string, object>>(line);
+            yield return currentItem;
+        }
     }
 
     private IEnumerable<RawEntry> GetColumnValues(string columnName)
@@ -41,16 +72,18 @@ class JsonFileTableWrapper : IFileTableWrapper
         using var fileStream = File.OpenRead(Filename);
         using var reader = new StreamReader(fileStream);
         var values = new HashSet<string>();
-        while (!reader.EndOfStream)
+        var firstChar = (char)reader.Read();
+        reader.DiscardBufferedData();
+        fileStream.Seek(0, SeekOrigin.Begin);
+        var items = firstChar == '[' ? GetItemsByArray(reader) : GetItemsByRow(reader);
+        foreach (var item in items)
         {
-            var line = reader.ReadLine();
-            var currentItem = JSON.Deserialize<Dictionary<string, object>>(line);
-            if (!currentItem.ContainsKey(columnName))
+            if (!item.ContainsKey(columnName))
             {
                 continue;
             }
 
-            var value = currentItem[columnName].ToString();
+            var value = item[columnName].ToString();
             if (values.Contains(value))
             {
                 continue;
